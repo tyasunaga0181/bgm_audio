ab31f9678357f87d2fcf2810ef96d1fba6fce303
tyasunaga0181 <67366372+tyasunaga0181@users.noreply.github.com>
2020-07-14 01:36:31 +0900

    Update musicnone.js

--- musicnone.js
+++ musicnone.js
@@ -1,14 +1,3 @@
-var PC_check = {
-  type: 'survey-multi-select',
-  questions: [
-    　{prompt:'<p>本実験はスマートフォンでは実施できません。必ずPCで行うようにしてください。PCで行っている場合はチェックをお願いします。</p>',
-      options: ['<span style = "font-size: 14pt">私はPCで本実験に参加しています。</span>'],
-      required: true,
-      name: 'approval_2'
-  }],
-  button_label: '次へ'
-};
-
 var pre_audio = {
   type: 'html-button-response',
   stimulus:　"<p style = 'text-align:left'>Continueを押すと音楽が再生されます。</p>" +
@@ -35,7 +24,6 @@ var pre_audio = {
   };
 
   var timeline = [];
-  timeline.push(PC_check);
   timeline.push(pre_audio);
   timeline.push(instructions);
   timeline.push(instructions_2);
